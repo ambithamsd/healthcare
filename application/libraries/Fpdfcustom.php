61fada1e1973f3397ad2bd5b53bc4fdbef6450ae
ambitha <you@example.com>
2022-03-22 01:50:13 +0530

    first commit

--- application/libraries/Fpdfcustom.php
+++ application/libraries/Fpdfcustom.php
@@ -14,7 +14,7 @@ class Fpdfcustom extends FPDF
 		// Move to the right
 		$this->Cell(80);
 		// Title
-		$this->Cell(30,10,'Title',1,0,'C');
+		$this->Cell(30,10,'Report',1,0,'C');
 		// Line break
 		$this->Ln(20);
 	}
